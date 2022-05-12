24c5b0b82e521531a124482515848d56315d623a
Eduardo <eduardodiasdini@gmail.com>
2022-05-12 08:21:47 -0300

    Moved to functions, dealing with asynchronous

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,28 +3,60 @@ import PlaygroundSupport
 
 PlaygroundPage.current.needsIndefiniteExecution = true
 
-struct DogId: Decodable {
-  let id: Int
-  let name: String
-}
+enum BrazilianDogBreeds: CaseIterable {
+    case shitzu
+    case poodle
+    case pug
+    case goldenRetriever
 
-let url = URL(string: "https://api.thedogapi.com/v1/breeds?limit=10&page=0")!
-
-var request = URLRequest(url: url)
-request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-
-let task = URLSession.shared.dataTask(with: url) { data, response, error in
-  if let data = data {
-    if let dogIdList = try? JSONDecoder().decode([DogId].self, from: data) {
-      dogIdList.forEach { dog in
-        print("\(dog.id): \(dog.name)")
-      }
-    } else {
-      print("Invalid response")
+    var breedString: String {
+        switch self {
+        case .shitzu:             return "Shih Tzu"
+        case .poodle:             return "Poodle (Toy)"
+        case .pug:                return "Pug"
+        case .goldenRetriever:    return "Golden Retriever"
+        }
     }
-  } else if let error = error {
-    print(error)
-  }
 }
 
-task.resume()
+struct DogId: Decodable, Equatable {
+    let id: Int
+    let name: String
+}
+
+func getBreedsList(completionBlock: @escaping ([DogId]) -> Void) {
+    let url = URL(string: "https://api.thedogapi.com/v1/breeds?limit=500&page=0")!
+
+    var request = URLRequest(url: url)
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+
+    let task = URLSession.shared.dataTask(with: url) { data, response, error in
+        if let data = data {
+            if let dogIdList = try? JSONDecoder().decode([DogId].self, from: data) {
+                completionBlock(dogIdList)
+            } else {
+                print("Invalid response")
+            }
+        } else if let error = error {
+            print(error)
+        }
+    }
+    task.resume()
+}
+
+func findBrazilianBreedsId(allBreedsList: [DogId]) -> [BrazilianDogBreeds: Int] {
+    var brazilianBreedById: [BrazilianDogBreeds: Int] = [:]
+    BrazilianDogBreeds.allCases.forEach { dog in
+        if let breed = allBreedsList.first(where: { $0.name == dog.breedString }) {
+            brazilianBreedById[dog] = breed.id
+        }
+    }
+    return brazilianBreedById
+}
+
+
+getBreedsList { (dogList) in
+    findBrazilianBreedsId(allBreedsList: dogList).forEach { dog in
+        print(dog)
+    }
+}
